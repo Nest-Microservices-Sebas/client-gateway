71930aed5825861f45d8189d42917d560e5db950
Sebastian <sebastianvalbuena25@gmail.com>
2025-01-05 20:22:49 -0500

    Update console.log main

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
 
   await app.listen(envs.port);
 
-  console.log('Hola mundo - primer cambio')
+  console.log('Hola mundo - segundo cambio')
 
   logger.log(`Running on port ${envs.port}`);
 }
